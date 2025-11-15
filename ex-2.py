7b814d53b0f2a66988a8897de1469a69fca8435d
aylin <aylin@aylin-MacBook-Air.local>
2025-11-15 21:33:46 +0300

    exercise2

--- ex-2.py
+++ ex-2.py
@@ -1,1 +1,13 @@
+#Write a function that lists all prime numbers less than or equal to a given positive integer.
+def list_primes(limit):
+    primes = []
+    for n in range(2, limit + 1):
+        for i in range(2, n):
+            if n % i == 0:
+                break
+        else:
+            primes.append(n)
+    return primes
 
+num = int(input("Enter a positive integer: "))
+print("Prime numbers up to", num, ":", list_primes(num))
