dbcd67f6677d3ddc7eed59b7072b20a7104ff3aa
aylin <aylin@aylin-MacBook-Air.local>
2025-11-15 21:35:50 +0300

    exercise3

--- ex-3.py
+++ ex-3.py
@@ -1,1 +1,25 @@
+#Use a loop and the function above to determine whether 2p−1 is prime for each prime number p not exceeding 100.
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
+def list_primes(limit):
+    primes = []
+    for n in range(2, limit + 1):
+        if is_prime(n):
+            primes.append(n)
+    return primes
+
+primes_up_to_100 = list_primes(100)
+
+
+for p in primes_up_to_100:
+    m = 2**p - 1
+    if is_prime(m):
+        print(f"2^{p} - 1 = {m} is prime")
+    else:
+        print(f"2^{p} - 1 = {m} is not prime")
