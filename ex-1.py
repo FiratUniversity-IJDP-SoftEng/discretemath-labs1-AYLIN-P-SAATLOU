295b5e604cad82dae547ceab314e0bbbcc46cf16
aylin <aylin@aylin-MacBook-Air.local>
2025-11-15 21:31:26 +0300

    exercise1

--- ex-1.py
+++ ex-1.py
@@ -1,1 +1,15 @@
+#Write a function that determines if a positive integer is prime.
 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(num, "is prime.")
+else:
+    print(num, "is not prime.")
